19b47cc8bde7c1ef0012e32b4e729018c4b7829e
VITALIY1986 <staslego1986@gmail.com>
2024-07-24 18:53:06 +0300

    sdfsddfdfsdf

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "64de73313cb98720"; }
+/******/ 		__webpack_require__.h = function() { return "d11d1fb53a7a33d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
